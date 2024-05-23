09338fb18ae9ccf603d12d234165087e14117e09
aytnat <tantya.saputra@gmail.com>
2024-05-24 01:27:13 +0700

    Fix authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::API
   def authenticate_request!
     token = JWT.decode(access_token, ENV.fetch('SECRET_KEY'), true, algorithm: 'HS256')
     user_id = token[0]['user_id']
-    user = User.find(user_id)
-    render json: user
+    @user = User.find(user_id)
+    raise HandledError::AuthenticationError, "invalid user" unless @user.present?
   end
 
   def authorization_header
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::API
   end
 
   def access_token
-    authorization_header.split(' ').second
+    authorization_header
   end
 end
--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -66,7 +66,7 @@ class AuthenticationsController < ApplicationController
   def login
     user = User.find_by(email: login_params[:email])
     if user&.authenticate(login_params[:password])
-      token = JWT.encode({ user_id: user.id }, ENV.fetch('SECRET'), 'HS256')
+      token = JWT.encode({ user_id: user.id }, ENV.fetch('SECRET_KEY'), 'HS256')
       render json: { user: { id: user.id, email: user.email }, token: token}
     else
       render json: { errors: 'invalid credentials!' }, status: :unprocessable_entity
