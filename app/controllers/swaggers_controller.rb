60c9825d2a84d70087a15c51d44fcfd6a32e18e1
aytnat <tantya.saputra@gmail.com>
2024-05-23 22:19:00 +0700

    Add swagger controller

---
+++ app/controllers/swaggers_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class SwaggersController < ActionController::Base
+  include Swagger::Blocks
+
+  swagger_root do
+    key :swagger, '2.0'
+    info do
+      key :version, '1.0.0'
+      key :title, 'Invoice API Documentation'
+      key :description, 'API Documentation for Invoice APPs'
+      contact do
+        key :name, 'Invoice API Team'
+      end
+      license do
+        key :name, 'MIT'
+      end
+    end
+
+    # key :host, 'petstore.swagger.wordnik.com'
+    # key :basePath, '/api'
+    key :consumes, ['application/json']
+    key :produces, ['application/json']
+  end
+
+  # A list of all classes that have swagger_* declarations.
+  SWAGGERED_CLASSES = [
+    ItemsController,
+    Item,
+    # AuthenticationController,
+    self,
+  ].freeze
+
+  def index
+    render json: Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :apidocs, only: [:index]
+  resources :swaggers, only: [:index]
   resources :items, only: %i[index create destroy show]
 end
