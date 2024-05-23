ee78a24acc8d6d882b930103109c55e21445db5a
aytnat <tantya.saputra@gmail.com>
2024-05-23 10:16:56 +0700

    Add models user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+class User < ApplicationRecord
+  has_secure_password
+  alias_attribute :password_digest, :password_hash
+
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+end
