93b2082a49f3dad94f351662b413e8d37eb11163
aytnat <tantya.saputra@gmail.com>
2024-05-24 01:09:42 +0700

    Add authenticate_request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include Helpers::ErrorHandler
+  def authenticate_request!
+    token = JWT.decode(access_token, ENV.fetch('SECRET_KEY'), true, algorithm: 'HS256')
+    user_id = token[0]['user_id']
+    user = User.find(user_id)
+    render json: user
+  end
+
+  def authorization_header
+    raise HandledError::AuthenticationError, "missing authorization header" unless request.headers["Authorization"].present?
+    request.headers["Authorization"]
+  end
+
+  def access_token
+    authorization_header.split(' ').second
+  end
 end
