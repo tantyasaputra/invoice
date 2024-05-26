890e5e75bbf97abbe7855dc769fb3f1680a1443f
aytnat <tantya.saputra@gmail.com>
2024-05-26 12:29:06 +0700

    Add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create users
+User.create!(first_name: 'admin', last_name: 'admin', email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+# Create items
+Item.create!(name: 'iPhone 13', description: 'Description for Phone 13', unit_price: 300.0)
+Item.create!(name: 'iPhone 14', description: 'Description for Phone 14', unit_price: 400.0)
+Item.create!(name: 'iPhone 15', description: 'Description for Phone 15', unit_price: 500.0)
